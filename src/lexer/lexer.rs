4e03e55f6d95770c49f34da1b57fcc800276edaa
Chau Nguyen <chautnguyen96@gmail.com>
2019-06-28 19:49:10 -0700

    lexer: Implement basic integer token lexing

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -30,28 +30,101 @@ impl Lexer {
         let current = '\0';
         let end = false;
 
-        return Lexer {
+        let mut lexer = Lexer {
             characters,
             position,
             current,
             end,
             diagnostics,
         };
+
+        lexer.read_next();
+
+        return lexer;
     }
 
     pub fn next(&mut self) -> Token {
-        self.read_next();
-        println!("current character: {}", self.current);
+        loop {
+            self.skip_blanks();
+            if self.end {
+                return Token::StaticToken { tag: Tag::End };
+            }
+
+            println!("current character: {}", self.current);
+
+            if self.current.is_digit(10) {
+                return self.read_integer_token();
+            }
+
+            if self.current.is_alphabetic() {
+                return self.read_identifier_token();
+            }
+
+            if self.current == '\"' {
+                return self.read_string_token();
+            }
+
+            match self.current {
+                _ => {
+                    println!("static token");
+                    self.read_next();
+                }
+            };
+        }
+
         return Token::StaticToken { tag: Tag::End };
     }
 
     fn read_next(&mut self) {
-        if (self.position as usize) > self.characters.len() {
-            println!("Strange error has occurred.");
+        if self.current != '\0' {
+            self.increment_position();
+        }
+        if (self.position as usize) >= self.characters.len() {
+            self.end = true;
         } else {
             self.current = self.characters[self.position as usize];
-            self.position += 1;
         }
+    }
+
+    fn skip_blanks(&mut self) {
+        while !self.end && (self.current == ' ' || self.current == '\n' || self.current == '\r') {
+            self.read_next();
+        }
+    }
+
+    fn read_integer_token(&mut self) -> Token {
+        let mut number = 0;
+        while !self.end && self.current.is_digit(10) {
+            number = (number * 10) + self.current.to_digit(10).unwrap();
+            // println!("{}", number);
+            self.read_next();
+        }
+        println!("return integer token");
+        return Token::IntegerToken {
+            value: (number as i32),
+        };
+    }
+
+    fn read_identifier_token(&mut self) -> Token {
+        while self.current.is_alphabetic() {
+            self.read_next();
+        }
+        return Token::IdentifierToken {
+            name: "empty identifier",
+        };
+    }
+
+    fn read_string_token(&mut self) -> Token {
+        while self.current != '\"' {
+            self.read_next();
+        }
+        return Token::StringToken {
+            value: "empty string",
+        };
+    }
+
+    fn increment_position(&mut self) {
+        self.position += 1;
     }
 }
 
@@ -63,15 +136,27 @@ mod tests {
     use crate::lexer::tokens::tag::Tag;
     use crate::lexer::tokens::token::Token;
 
-    // This is for testing any sort of code. No asserts should be in here.
     #[test]
-    fn test_playground() {
-        let source = "class /* Comment */ A { }";
+    fn test_playground_1() {
+        let source = "class A { }";
         let diagnostics = Diagnostics::make();
         let mut lexer = Lexer::make(source, diagnostics);
-        lexer.next();
-        lexer.next();
-        lexer.next();
+        assert_static_token(Tag::Class, lexer.next());
+        assert_identifier_token("A", lexer.next());
+        assert_static_token(Tag::OpenBrace, lexer.next());
+        assert_static_token(Tag::CloseBrace, lexer.next());
+        assert_static_token(Tag::End, lexer.next());
+        assert!(lexer.diagnostics.has_no_errors());
+    }
+
+    #[test]
+    fn test_playground_2() {
+        let source = "123";
+        let diagnostics = Diagnostics::make();
+        let mut lexer = Lexer::make(source, diagnostics);
+        assert_integer_token(123, lexer.next());
+        assert_static_token(Tag::End, lexer.next());
+        assert!(lexer.diagnostics.has_no_errors());
     }
 
     #[test]
@@ -185,6 +270,7 @@ mod tests {
     }
 
     fn assert_integer_token(expected_value: i32, actual: Token) {
+        println!("{} {:?}", expected_value, actual);
         assert!(actual.is_integer());
 
         match actual {
